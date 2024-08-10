4afda73e840448fa136ffb1f68b7ac9dbb4e431b
anggasatriabima <anggasatriabima93@gmail.com>
2024-08-11 04:45:38 +0700

    refaktor

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -51,9 +51,6 @@ export const CardBlog: React.FC<ICardBlog> = ({
           className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-500 rounded-lg hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           See more
-          {/* <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                  </svg> */}
         </Link>
       </div>
     </div>
